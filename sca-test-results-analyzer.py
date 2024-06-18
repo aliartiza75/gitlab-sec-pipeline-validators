b7be8692160d4696b6491b87b68d0691ea271a6d
Irtiza Ali <irtiza@stakater.com>
2024-06-18 23:27:37 +0200

    Create sca-test-results-analyzer.py

---
+++ sca-test-results-analyzer.py
@@ -0,0 +1,8 @@
+# It analyzes the results of opensca-cli result file in a Gitlab Job. In case of any issues found, it will raise an error. 
+
+import json
+f = open("sca.json")
+data = json.load(f)
+if ("indirect_vulnerabilities" in list(data)) and ((data["indirect_vulnerabilities"] > 0)):
+    raise Exception("SCA Tool detected issues")
+f.close()
