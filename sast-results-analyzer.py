1dbeb7c4a6bcd206bca17ab388a8b32b15875c55
Irtiza Ali <irtiza@stakater.com>
2024-06-19 23:31:52 +0200

    Update sast-results-analyzer.py

--- sast-results-analyzer.py
+++ sast-results-analyzer.py
@@ -1,4 +1,4 @@
-# It analyze the results of sonar cloud sast analysis result file in a Gitlab Job. In case of any issues found in the measure, i.e, code_smells, bugs, vulnerabilitie, it will raise an error. 
+# It analyze the results of sonar cloud  sast analysis result file in a Gitlab Job. In case of any issues found in the measure, i.e, code_smells, bugs, vulnerabilitie, it will raise an error. 
 # API Endpoint to get the results
 # curl -s -u "${SONAR_TOKEN}:" "https://sonarcloud.io/api/measures/component?component=<project-id>&metricKeys=code_smells,bugs,vulnerabilities&branch=${CI_COMMIT_REF_NAME}" > sast-result.json
 
