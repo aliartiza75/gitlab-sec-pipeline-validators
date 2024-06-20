cdc56ae0028695eca81d2e4c09792df52f20564a
Irtiza Ali <irtiza@stakater.com>
2024-06-20 15:58:27 +0200

    Update secret-detection-results-analyzer.py

--- secret-detection-results-analyzer.py
+++ secret-detection-results-analyzer.py
@@ -1,8 +1,9 @@
-# It analyzes the results of the detect-secrets (python package) result file in a Gitlab Job. In case of any issues found in the measure, it will raise an error. 
+# It analyzes the results of the detect-secrets (python package) result file in a Gitlab Job. In case of any issues in the result, it will raise an exception. 
 
 import json
 f = open("secret-detection-job.json")
 data = json.load(f)
+# In case of any secret detection, the result key in the json is going to have values in it.
 if len(list(data["results"])) > 0:
     raise Exception("Secrets found, details can be found in the job artifact")
 f.close()
